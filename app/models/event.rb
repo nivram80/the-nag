4de149a5e9c2918df9f0c8876f3b64d79fe3f166
Todd Nichols <nivram80@gmail.com>
2014-03-25 13:14:17 -0500

    add error messaging throws error everytime

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   attr_accessible :name, :user_id, :due
   has_many :groups
-  validates :due, date: { after: Proc.new { Time.now } }
+  validates :due, date: { after: Proc.new { Time.now }, message: 'must be after today' }, on: :create
 end
