8cdc29cf5a6b8c5ee99dacd68fd1f705f72ccb62
Todd Nichols <nivram80@gmail.com>
2014-03-26 17:51:31 -0500

    add event dropdown on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,12 @@
 class DashboardsController < ApplicationController
 
 	def dashboard
-		@events = Event.where("user_id = ?", session[:user_id]).order("due")
+		if params[:event]
+			@selected_event = Event.find(params[:event][:id])
+		else
+			@selected_event = Event.where("user_id = ?", session[:user_id]).order("due").first
+		end
 		@event = Event.new
-
 		#@groups = Group.event.where("event.id = ?", session[:user_id])
 
 		@task = Task.new
