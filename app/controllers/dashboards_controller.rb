527d3370ac4937cd770b124867904ebb099aea4c
Todd Nichols <nivram80@gmail.com>
2014-03-25 22:16:07 -0500

    intial setup for adding groups and tasks to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,7 @@ class DashboardsController < ApplicationController
 	def dashboard
 		@events = Event.where("user_id = ?", session[:user_id] ).order("due")
 		@event = Event.new
+		@groups = Group.where("event_id = ?", @events.id)
 	end
 
 end
