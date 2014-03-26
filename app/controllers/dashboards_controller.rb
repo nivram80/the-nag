ee8d86edb603a3b0f76a0419aa8919b81666730a
Todd Nichols <nivram80@gmail.com>
2014-03-25 23:04:03 -0500

    list groups with associated tasks on dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,12 @@
 class DashboardsController < ApplicationController
 
 	def dashboard
-		@events = Event.where("user_id = ?", session[:user_id] ).order("due")
+		@events = Event.where("user_id = ?", session[:user_id]).order("due")
 		@event = Event.new
-		@groups = Group.where("event_id = ?", @events.id)
+
+		#@groups = Group.event.where("event.id = ?", session[:user_id])
+
+		#@tasks = Task.where("group_id = ?", @group)
 	end
 
 end
