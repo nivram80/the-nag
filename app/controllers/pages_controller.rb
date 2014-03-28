ae59a9c6ae4d0954c76b06a6e926bdec80bb1ce1
danielwells <danielwells.usnret@gmail.com>
2014-03-28 00:43:31 -0500

    Fixed problem with redirecting to the landing page after a failed login attempt. It now goes back to the login page with a note to the user and an option to create a new account.  Also redirected to the dashboard if the landing page is loaded with a user already logged in.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   protect_from_forgery
   
   def home
-    
+    # binding.pry
+    if session[:user_id]
+      # binding.pry
+      redirect_to :dashboard
+    end
   end
     
 end
